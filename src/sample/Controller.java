c1e3e4a713391655721e1609ea48697020bfc817
Nelson Daniel Durán <ndanield@hotmail.com>
2017-06-25 10:48:44 -0400

    Nuevos enlaces en la interfaz. Nuevos metodos de accion. Función de abrir archivos agregada.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,62 +2,103 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.Utils;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.trees.J48;
 import weka.classifiers.Evaluation;
-import java.util.Random;
 
-public class Controller {
+import java.io.File;
+import java.net.URL;
+import java.util.Random;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
 
     @FXML
-    private Button btnClasify = new Button();
+    private Button btnClassify = new Button();
+
+    @FXML
+    private Button btnOpen = new Button();
 
     @FXML
     private TextArea resultArea = new TextArea();
 
     @FXML
-    private void handleButtonAction(ActionEvent e) {
-        if (e.getSource() == btnClasify)
-        {
-            try {
-                classify();
-            } catch (Exception e1) {
-                e1.printStackTrace();
-            }
+    private Label lblOpenedFile = new Label();
+
+    private final FileChooser fileChooser = new FileChooser();
+    private File file;
+
+    @FXML
+    private void handleButtonOpen() {
+        btnClassify.setDisable(false);
+
+
+        file = fileChooser.showOpenDialog(btnClassify.getScene().getWindow());
+        lblOpenedFile.setText(file.getName());
+    }
+
+    @FXML
+    private void handleButtonClassify() {
+        try {
+            classify(file.getAbsolutePath());
+        } catch (Exception e1) {
+            e1.printStackTrace();
         }
     }
 
-    private void classify() throws Exception {
-        DataSource source = new DataSource("datasets/weather.nominal.arff");
+    private void classify(String path) throws Exception {
+
+
+        DataSource source = new DataSource(path);
         Instances data = source.getDataSet();
 
         if (data.classIndex() == -1)
             data.setClassIndex(data.numAttributes() - 1);
+
+        /*
+        String atributosNewInst = "rainy,mild,high,FALSE,yes";
+        String [] atributos = atributosNewInst.split(",");
+
+        Instance inst = new DenseInstance(data.numAttributes());
+        for (int i = 0; i < data.numAttributes(); i++) {
+            inst.setValue(i, atributos[i]);
+        }
+        data.add(inst);
+        */
 
         J48 tree = new J48();
         tree.buildClassifier(data);
         Evaluation eval = new Evaluation(data);
         eval.crossValidateModel(tree, data, 10, new Random(1));
 
+
         StringBuffer        result;
 
         result = new StringBuffer();
-        result.append("Weka - Demo\n===========\n\n");
+        result.append("\n================================\nPrueeeeeeeeba\n===============================\n\n");
 
-        result.append("Classifier...: "
-                + tree.getClass().getName() + " "
-                + Utils.joinOptions(tree.getOptions()) + "\n");
-
+        result.append("Classifier...: " + tree.getClass().getName() +"\n");
+        result.append(eval.toClassDetailsString() + "\n");
         result.append(tree.toString() + "\n");
         result.append(eval.toSummaryString() + "\n");
-
         result.append(eval.toMatrixString() + "\n");
-        result.append(eval.toClassDetailsString() + "\n");
+
 
         resultArea.setText(result.toString());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        btnClassify.setDisable(true);
+        fileChooser.setInitialDirectory(new File("C:\\Users\\NelsonDaniel\\workspace\\Wekaface\\datasets"));
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,12 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("WekaFace");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.setMaximized(true);
         primaryStage.show();
