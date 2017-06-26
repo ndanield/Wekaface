2944ae97cfc3cfa5bd576f0d46724454121f84a5
Leonardo Castro <leonardoaii71@gmail.com>
2017-06-25 20:34:37 -0400

    new efforts added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,26 +1,28 @@
 package sample;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.*;
 import javafx.stage.FileChooser;
-import weka.core.DenseInstance;
-import weka.core.Instance;
-import weka.core.Instances;
-import weka.core.Utils;
+import javafx.stage.Stage;
+import weka.core.*;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.trees.J48;
 import weka.classifiers.Evaluation;
 
 import java.io.File;
 import java.net.URL;
+import java.util.Enumeration;
 import java.util.Random;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
 
     @FXML
     private Button btnClassify = new Button();
@@ -37,6 +39,36 @@ public class Controller implements Initializable{
     private final FileChooser fileChooser = new FileChooser();
     private File file;
 
+    static String filePath;
+
+    ObservableList tablemodel = FXCollections.observableArrayList();
+
+    @FXML
+    TableView<ObservableList<String>> tableView = new TableView<>();
+
+//controles de la instancia
+
+    @FXML
+    private JFXTextField rainyF;
+
+    @FXML
+    private JFXTextField coolF;
+
+    @FXML
+    private JFXTextField highF;
+
+    @FXML
+    private JFXTextField falseF;
+
+    @FXML
+    private JFXTextField yesF;
+
+    @FXML
+    private JFXTextField agregarF;
+
+
+
+
     @FXML
     private void handleButtonOpen() {
         btnClassify.setDisable(false);
@@ -44,12 +76,23 @@ public class Controller implements Initializable{
 
         file = fileChooser.showOpenDialog(btnClassify.getScene().getWindow());
         lblOpenedFile.setText(file.getName());
+        filePath = file.getAbsolutePath();
+
     }
 
     @FXML
     private void handleButtonClassify() {
         try {
-            classify(file.getAbsolutePath());
+            classify(filePath);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void handleButtonAgregar() {
+        try {
+            classify(filePath);
         } catch (Exception e1) {
             e1.printStackTrace();
         }
@@ -60,12 +103,15 @@ public class Controller implements Initializable{
 
         DataSource source = new DataSource(path);
         Instances data = source.getDataSet();
+        //tablemodel.add(data);
+        //tableView.getItems().addAll(tablemodel);
+
 
         if (data.classIndex() == -1)
             data.setClassIndex(data.numAttributes() - 1);
 
 
-        String [] atributos = {"rainy", "cool", "high", "FALSE", "yes"};
+        String[] atributos = {"rainy", "cool", "high", "FALSE", "yes"};
         Instance inst = new DenseInstance(data.numAttributes());
 
         inst.setDataset(data);
@@ -82,7 +128,7 @@ public class Controller implements Initializable{
         StringBuilder result = new StringBuilder();
         for (int i = 0; i < data.numInstances(); i++) {
             double pred = tree.classifyInstance(data.instance(i));
-            result.append("No. " + (i+1) + "\n");
+            result.append("No. " + (i + 1) + "\n");
             result.append(data.instance(i).toString() + "\n");
             result.append("Valor predecido: " + data.classAttribute().value((int) pred) + "\n");
             result.append("Valor real: " + data.classAttribute().value((int) data.instance(i).classValue()) + "\n\n");
@@ -101,11 +147,33 @@ public class Controller implements Initializable{
 
         resultArea.setText(result.toString());
 
+
+
+    for( int i = 0; i<data.numAttributes(); i++) {
+        final int finalIdx = i;
+        TableColumn<ObservableList<String>, String> column = new TableColumn<>(
+                data.attribute(finalIdx).name()
+        );
+        column.setCellValueFactory(param ->
+                new ReadOnlyObjectWrapper<>(param.getValue().get(finalIdx))
+        );
+        tableView.getColumns().add(column);
     }
+
+
+
+}
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btnClassify.setDisable(true);
-        fileChooser.setInitialDirectory(new File("C:\\Users\\NelsonDaniel\\workspace\\Wekaface\\datasets"));
+        //fileChooser.setInitialDirectory(new File("C:\\Users\\NelsonDaniel\\workspace\\Wekaface\\datasets"));
     }
+
+
+
+
+
+
+
 }
